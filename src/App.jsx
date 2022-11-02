6a9dc2e3f09c6f4eb79e7e5be3ad63cd86611546
ParmezhovSergey <parmezhovsergey@mail.ru>
2022-11-02 18:23:01 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import MainPage from "./components/MainPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/3davinci" element={<MainPage />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
